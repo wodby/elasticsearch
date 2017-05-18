304cfcf427958dc9d7df5a7504a4455280c8627c
Chingis Sandanov <chingis@wodby.com>
2017-05-18 12:50:00 +0700

    Debug travis build

--- 5.4/test.sh
+++ 5.4/test.sh
@@ -9,12 +9,13 @@ fi
 name=$1
 image=$2
 
-cid="$(docker run -d --name "${name}" "${image}")"
-trap "docker rm -vf $cid > /dev/null" EXIT
-
-elasticsearch() {
-	docker run --rm -i --link "${name}":"elasticsearch" "${image}" "${@}" host="elasticsearch"
-}
-
-elasticsearch make check-ready wait_seconds=10 max_try=12
+docker run --name "${name}" "${image}"
+#cid="$(docker run -d --name "${name}" "${image}")"
+#trap "docker rm -vf $cid > /dev/null" EXIT
+#
+#elasticsearch() {
+#	docker run --rm -i --link "${name}":"elasticsearch" "${image}" "${@}" host="elasticsearch"
+#}
+#
+#elasticsearch make check-ready wait_seconds=10 max_try=12
 
